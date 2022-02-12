4e2d83e8c7d766fa2c3b6ba1ad599c44f88b5f65
start.vaadin.com <no-reply@vaadin.com>
2022-02-12 11:19:52 +0000

    Generated project

---
+++ src/main/java/com/mydeveloperplanet/myvaadinplanet/views/showevents/ShoweventsView.java
@@ -0,0 +1,31 @@
+package com.mydeveloperplanet.myvaadinplanet.views.showevents;
+
+import com.mydeveloperplanet.myvaadinplanet.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Showevents")
+@Route(value = "showevents", layout = MainLayout.class)
+public class ShoweventsView extends VerticalLayout {
+
+    public ShoweventsView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
